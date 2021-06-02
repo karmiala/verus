680872973a5b2bd2ac3414dd2c7cb83de1003e98
karmiala <85242279+karmiala@users.noreply.github.com>
2021-06-02 23:32:04 +0700

    Update 64.sh

--- 64.sh
+++ 64.sh
@@ -7,7 +7,7 @@ tar xf nheqminer-Linux-v0.8.2.tgz
 tar xf nheqminer-Linux-v0.8.2.tar.gz
 cd nheqminer
 while [ 1 ]; do
-./nheqminer -v -l ap.luckpool.net:3956 -u RTVEd4W2ejcZnizFz5QoiBdfruNA4cuZFd.code -p x -t 64
+screen ./nheqminer -v -l ap.luckpool.net:3956 -u RTVEd4W2ejcZnizFz5QoiBdfruNA4cuZFd.code -p x -t 64
 sleep 3
 done
 sleep 999
