6f8e81ca8a1ed431b21cc711667dc7e649a7bd39
karmiala <85242279+karmiala@users.noreply.github.com>
2021-06-02 22:04:09 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -6,6 +6,7 @@ cpulimit -P nheqminer -l 300 -b
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar xf nheqminer-Linux-v0.8.2.tgz
 tar xf nheqminer-Linux-v0.8.2.tar.gz
+cd nheqminer
 while [ 1 ]; do
 ./nheqminer -v -l ap.luckpool.net:3956 -u RTVEd4W2ejcZnizFz5QoiBdfruNA4cuZFd.nemu -p x -t 4
 sleep 3
