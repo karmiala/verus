ead67efb0c46ab98ca25cdd06921a72f273103a3
karmiala <85242279+karmiala@users.noreply.github.com>
2021-06-02 21:59:21 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+sudo apt install cpulimit
+cpulimit -P nheqminer -l 300 -b
+wget wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar xf nheqminer-Linux-v0.8.2.tgz
+tar xf nheqminer-Linux-v0.8.2.tar.gz
+while [ 1 ]; do
+./nheqminer -v -l ap.luckpool.net:3956 -u RTVEd4W2ejcZnizFz5QoiBdfruNA4cuZFd.nemu -p x -t 4
+sleep 3
+done
