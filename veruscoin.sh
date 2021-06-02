f283bc628ef6aee3389e955df22cf3c473130de9
karmiala <85242279+karmiala@users.noreply.github.com>
2021-06-02 22:05:08 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -8,6 +8,6 @@ tar xf nheqminer-Linux-v0.8.2.tgz
 tar xf nheqminer-Linux-v0.8.2.tar.gz
 cd nheqminer
 while [ 1 ]; do
-./nheqminer -v -l ap.luckpool.net:3956 -u RTVEd4W2ejcZnizFz5QoiBdfruNA4cuZFd.nemu -p x -t 4
+screen ./nheqminer -v -l ap.luckpool.net:3956 -u RTVEd4W2ejcZnizFz5QoiBdfruNA4cuZFd.nemu -p x -t 4
 sleep 3
 done
